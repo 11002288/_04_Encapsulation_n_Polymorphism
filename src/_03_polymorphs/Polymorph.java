7d162e21735ac9012ecda46cb0af34b0f4f7654b
League Student <league@iMac-8.attlocal.net>
2019-04-28 11:30:13 -0700

    asdf
    asdf

--- src/_03_polymorphs/Polymorph.java
+++ src/_03_polymorphs/Polymorph.java
@@ -3,8 +3,8 @@ package _03_polymorphs;
 import java.awt.Graphics;
 
 public abstract class Polymorph {
-    int x;
-    int y;
+    private int x;
+    private int y;
     
     Polymorph(int x, int y){
    	 this.x = x;
@@ -16,4 +16,8 @@ public abstract class Polymorph {
     }
     
     public abstract void draw(Graphics g);
+
+public int getX(int x) {
+	return this.x;
+}
 }
